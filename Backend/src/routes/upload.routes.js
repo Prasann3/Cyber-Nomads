eb9600b400aecdb615c0b26db35abe61d23d2a1d
Prasann Trivedi <prasanntrivedi6263@gmail.com>
2025-11-06 22:22:44 +0530

    New Routes Added

--- Backend/src/routes/upload.routes.js
+++ Backend/src/routes/upload.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { uploadMany } from "../utils/multer.js";
-import { uploadInvoice , getAllUploads , getUploadById} from "../controllers/upload.controller.js";
+import { uploadInvoice , getAllUploads , getUploadById , exportUpload} from "../controllers/upload.controller.js";
 import { isLoggedIn } from "../middlewares/auth.middlewares.js";
 const router = Router();
 
 router.get("/" , getAllUploads)
       .get("/:id" , getUploadById)
+      .get("/:id/export" , exportUpload)
       .post("/", isLoggedIn, uploadMany, uploadInvoice);
 
 export default router;
