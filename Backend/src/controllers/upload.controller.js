ff7623e19ef348f9d2b38cbe0abdf0f1df7b7623
Prasann Trivedi <prasanntrivedi6263@gmail.com>
2025-11-06 18:34:48 +0530

    Updates Added

--- Backend/src/controllers/upload.controller.js
+++ Backend/src/controllers/upload.controller.js
@@ -1,64 +1,5 @@
 import express from 'express'
 import Upload from './../models/upload.model'
-
-exports.getAllUploads = async(req , res , next) => {
-    
-       try {
-
-         const uploads = await Upload.find().populate({
-            path : "invoiceId" ,
-            select : "vendor invoiceDetails totalInvoiceValue totalGSTValue status"
-         })
-
-         res.status(200).json(
-            {
-                status : "Success" ,
-                data : uploads
-            }
-         )
-
-       }
-       catch(err) {
-
-        res.status(500).json( 
-            {
-                status : "Failed" ,
-                error : err
-            }
-        )
-
-       }
-
-}
-
-
-exports.getUploadById = async (req , res , next) => {
-    
-    try {
-
-        const uploads = await Upload.findById(req.params.id).populate({
-            path : "invoiceId" ,
-            select : "vendor invoiceDetails totalInvoiceValue totalGSTValue status"
-         })
-
-         res.status(200).json(
-            {
-                status : "Success" ,
-                data : uploads
-            }
-         )
-
-    }
-    catch(err) {
-        res.status(500).json(
-            {
-                status : "Failed" ,
-                error : err
-            }
-        )
-    }
-
-}
 import { Queue } from "bullmq";
 import "dotenv/config";
 import Invoice from "../models/invoice.model.js";
@@ -123,3 +64,62 @@ export const uploadInvoice = async (req, res) => {
     });
   }
 };
+
+exports.getAllUploads = async(req , res , next) => {
+    
+       try {
+
+         const uploads = await Upload.find().populate({
+            path : "invoiceId" ,
+            select : "vendor invoiceDetails totalInvoiceValue totalGSTValue status"
+         })
+
+         res.status(200).json(
+            {
+                status : "Success" ,
+                data : uploads
+            }
+         )
+
+       }
+       catch(err) {
+
+        res.status(500).json( 
+            {
+                status : "Failed" ,
+                error : err
+            }
+        )
+
+       }
+
+}
+
+
+exports.getUploadById = async (req , res , next) => {
+    
+    try {
+
+        const uploads = await Upload.findById(req.params.id).populate({
+            path : "invoiceId" ,
+            select : "vendor invoiceDetails totalInvoiceValue totalGSTValue status"
+         })
+
+         res.status(200).json(
+            {
+                status : "Success" ,
+                data : uploads
+            }
+         )
+
+    }
+    catch(err) {
+        res.status(500).json(
+            {
+                status : "Failed" ,
+                error : err
+            }
+        )
+    }
+
+}
